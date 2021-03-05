27a991dc4d4928a0615b23219e38ac6882d4da01
Tomo Bessho <tbessho2@unl.edu>
2021-03-05 10:18:10 -0600

    I think the model is working with preprocessing now. Need to fix model.fit issue

--- code/main.py
+++ code/main.py
@@ -9,17 +9,18 @@ import util
 # getting the data set
 MAX_SEQ_LEN = 128
 MAX_TOKENS = 5000
-(x_train, y_train), (x_test, y_test) = util.getDataset(MAX_TOKENS, MAX_SEQ_LEN)
-x_train = tf.cast(x_train, tf.float32)
-y_train = tf.cast(y_train, tf.float32)
+# (x_train, y_train), (x_test, y_test) = util.getDataset(MAX_TOKENS, MAX_SEQ_LEN)
+ds = util.getDS()
 
 # getting our model
-EMBEDDING_SIZE = 32
 DROPOUT_RATE = 0.5
 REG_CONSTANT = 0.01
+BATCH_SIZE = 64
+
 model = model.define_rnn(
-    EMBEDDING_SIZE, MAX_TOKENS, MAX_SEQ_LEN, DROPOUT_RATE, REG_CONSTANT
+    MAX_TOKENS, MAX_SEQ_LEN, DROPOUT_RATE, REG_CONSTANT, BATCH_SIZE, ds
 )
+
 
 # compiling our model!
 model.compile(
@@ -32,15 +33,12 @@ model.compile(
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3)
 
 # training our model
-BATCH_SIZE = 64
 EPOCHS = 3
 history = model.fit(
-    x_train,
-    y_train,
+    ds["train"],
     batch_size=BATCH_SIZE,
     epochs=EPOCHS,
     callbacks=[early_stopping],
-    validation_split=0.1,
 )
 
 util.print_arch(model)
@@ -52,7 +50,78 @@ PATH = "./models/rnn_v" + str(VERSION)
 tf.keras.Model.save(model, PATH)
 
 # evaluating our model
-evaluate = model.evaluate(x_test, y_test, verbose=0)
+evaluate = model.evaluate(ds["test"], verbose=0)
 print("test accuracy: ", evaluate[1])
 
 util.graph_one(history, VERSION)
+
+######################################################################################
+
+
+# obtaining our model
+# DROPOUT_RATE = 0.5
+# REG_CONSTANT = 0.01
+# model = model.define_sequential_rnn(
+#     MAX_TOKENS, MAX_SEQ_LEN, REG_CONSTANT, DROPOUT_RATE, ds
+# )
+
+# # testing a forward pass
+# util.forward_pass(ds, model)
+
+# # setting up parameters for training
+# loss_values = []
+# accuracy_values = []
+# BATCH_SIZE = 32
+# EPOCHS = 1
+# LEARNING_RATE = 0.001
+# optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+
+# # Loop through epochs of data
+# for epoch in range(EPOCHS):
+#     for batch in tqdm(ds["train"].batch(BATCH_SIZE)):
+#         with tf.GradientTape() as tape:
+#             # run network
+#             text = batch["text"]
+#             labels = batch["label"]
+#             logits = model(text)
+
+#             # calculate loss
+#             loss = tf.keras.losses.binary_crossentropy(
+#                 tf.expand_dims(batch["label"], -1), logits, from_logits=True
+#             )
+#         loss_values.append(loss)
+
+#         # gradient update
+#         grads = tape.gradient(loss, model.trainable_variables)
+#         optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+#         # calculate accuracy
+#         predictions = tf.argmax(logits, axis=1)
+#         accuracy = tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+#         accuracy_values.append(accuracy)
+
+# print(model.summary())
+
+# # accuracy
+# print("Accuracy:", np.mean(accuracy_values))
+
+# loss_values_eval = []
+# accuracy_values_eval = []
+# # evaluating the model
+# for batch in tqdm(ds["test"].batch(BATCH_SIZE)):
+#     text = batch["text"]
+#     labels = batch["label"]
+#     logits = model(text)
+
+#     # calculate loss
+#     loss = tf.keras.losses.binary_crossentropy(
+#         tf.expand_dims(batch["label"], -1), logits, from_logits=True
+#     )
+#     loss_values_eval.append(loss)
+#     # calculate accuracy
+#     predictions = tf.argmax(logits, axis=1)
+#     accuracy = tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+#     accuracy_values_eval.append(accuracy)
+
+# # accuracy
+# print("Evaluated acuracy:", np.mean(accuracy_values_eval))
